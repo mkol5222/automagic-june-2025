cee1328759a76e32f16fa2c227d0322f3531d6db
Martin Koldovsky <mkoldov@gmail.com>
2025-08-28 08:17:49 +0000

    vwan

---
+++ vwan/sic.tf
@@ -0,0 +1,13 @@
+
+# random password 16 chars - numbers, letters
+resource "random_password" "sic_key" {
+  length  = 16
+  special = false
+}
+
+output sic_key {
+  value       = random_password.sic_key.result
+  sensitive   = true
+  description = "SIC Key"
+  depends_on  = [random_password.sic_key]
+}
