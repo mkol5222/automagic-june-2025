0b073209124e4b866f615153d207517e61ed52ae
Martin Koldovsky <mkoldov@gmail.com>
2025-09-01 07:44:57 +0000

    accept eula

--- scripts/cpman-up.sh
+++ scripts/cpman-up.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+
+az vm image terms accept \
+  --publisher checkpoint \
+  --offer check-point-cg-r82 \
+  --plan mgmt-byol -o table
+
 (cd management/terraform; terraform init)
 
 (cd management/terraform; terraform apply -auto-approve)
