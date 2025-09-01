d11e3e43a060bab8a1c0f67658d0b5bf8194ac12
Martin Koldovsky <mkoldov@gmail.com>
2025-09-01 15:32:03 +0000

    rg names

--- vwan/spokes.tf
+++ vwan/spokes.tf
@@ -4,7 +4,7 @@ module "spoke77" {
   source              = "./spoke"
   vnet_address        = "10.77.0.0/16"
   vnet_name           = "myVNet77"
-  vnet_rg             = "vnet_rg77"
+  vnet_rg             = "automagic-vnet_rg77"
   vwan_hub_rg        = "automagic-vwan-${local.secrets.envId}"
   vwan_hub_name      = "am-vwan-hub"
 }
@@ -13,7 +13,7 @@ module "spoke68" {
   source              = "./spoke"
   vnet_address        = "10.68.0.0/16"
   vnet_name           = "myVNet68"
-  vnet_rg             = "vnet_rg68"
+  vnet_rg             = "automagic-vnet_rg68"
   vwan_hub_rg        = "automagic-vwan-${local.secrets.envId}"
   vwan_hub_name      = "am-vwan-hub"
 }
