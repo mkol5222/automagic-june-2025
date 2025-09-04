3bcb028a1148867a567f8dd63d8c97a4a6eb30f6
Martin Koldovsky <mkoldov@gmail.com>
2025-09-04 09:54:45 +0000

    pol vnet68

--- policy/terraform/vnet68/main.tf
+++ policy/terraform/vnet68/main.tf
@@ -8,3 +8,38 @@ resource "checkpoint_management_access_layer" "layer68out" {
 
   
 }
+
+resource "checkpoint_management_access_rule" "layer68_rule_egress" {
+
+   layer       = "vmss Network"
+  position =  {top = "top"} // {top = "top"} // { above = checkpoint_management_access_rule.from_net_linux.id }
+  // {top = "top"} // { above = checkpoint_management_access_rule.from_net_linux.id }
+
+  name = "vnet68 egress"
+
+  source = ["net_vnet_spoke68"]//[checkpoint_management_network.spoke68.id]
+
+  enabled = true
+
+  destination        =["Any"]
+  destination_negate = false
+
+  service        = ["Any"]
+  service_negate = false
+
+  action       = "Apply layer"
+  inline_layer = "${checkpoint_management_access_layer.layer68out.id}"
+
+  #   action_settings = {
+  #     enable_identity_captive_portal = false
+  #   }
+
+  track = {
+    accounting              = false
+    alert                   = "none"
+    enable_firewall_session = true
+    per_connection          = true
+    per_session             = true
+    type                    = "Log"
+  }
+}
