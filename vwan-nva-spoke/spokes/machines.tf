966a9edabbbbef5a9eb36fb06a822dbb693c40c2
Martin Koldovsky <mkoldov@gmail.com>
2025-09-06 08:25:37 +0000

    spokes wvan2

---
+++ vwan-nva-spoke/spokes/machines.tf
@@ -0,0 +1,21 @@
+
+
+module "linux77" {
+    source = "./linux"
+    subnet_id = module.spoke77.subnet_id
+    vm_name = "linux77"
+    # vm_size = "Standard_DS1_v2"
+    linux_rg_name = "automagic-linux77-rg"
+    linux_location = "North Europe"
+}
+
+
+
+module "linux68" {
+    source = "./linux"
+    subnet_id = module.spoke68.subnet_id
+    vm_name = "linux68"
+    # vm_size = "Standard_DS1_v2"
+    linux_rg_name = "automagic-linux68-rg"
+    linux_location = "North Europe"
+}
---
+++ vwan-nva-spoke/spokes/variables.tf
@@ -0,0 +1,10 @@
+
+# variable "vwan_hub_rg" {
+#     description = "Resource group for the VWAN hub"
+#     default     = "automagic-vwan-0708a60e"
+# }
+
+# variable "vwan_hub_name" {
+#     description = "Name of the VWAN hub"
+#     default     = "am-vwan-hub"
+# }
